1a5db0afd24df11442403e049b9bc7925efeb046
Dmitry Gerasimov <udeadway523@gmail.com>
2024-01-30 21:04:58 +0300

    add useFetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import { useState } from 'react';
-import './App.css';
-import { useThrottle } from './useThrottle';
+import { useFetch } from './useFetch';
 
 function App() {
-  const [value, setValue] = useState("")
-  const throttledValue = useThrottle(value)
-    
+  const {
+    data,
+    isLoading,
+    error,
+    refetch
+  } = useFetch('https://jsonplaceholder.typicode.com/posts');
+	
   return (
-    <div className="App">
-      <header className="App-header">
-        <input value={value} onChange={(e) => setValue(e.target.value)}></input>
-        <p>
-            { throttledValue }
-        </p>
-      </header>
+    <div>
+      <div>
+        <button onClick={() => refetch({
+          params: {
+            _limit: 3
+          }
+        })}>
+          Перезапросить
+        </button>
+      </div>
+      {isLoading && 'Загрузка...'}
+      {error && 'Произошла ошибка'}
+      {data && !isLoading && data.map(item => <div key={item.id}>{item.title}</div>) }
     </div>
   );
 }
+
 
 export default App;
